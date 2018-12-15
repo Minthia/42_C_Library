fffcbd314d8125f89102e7404f7283a57c119751
hdo-minh <dominhthihugo@gmail.com>
2018-12-15 13:16:22 +0100

    Update ft_memset, opti

--- src/mem/ft_memset.c
+++ src/mem/ft_memset.c
@@ -14,7 +14,26 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	while (n--)
-		((unsigned char*)s)[n] = (unsigned char)c;
-	return ((void *)s);
+	long		*ptr;
+	char		*ptr2;
+	long		data;
+	size_t		i;
+
+	c = (unsigned char)c;
+	data = ((long)c << 56) + ((long)c << 48) + ((long)c << 40) + \
+	((long)c << 32) + (c << 24) + (c << 16) + (c << 8) + c;
+	ptr = (long *)ptr;
+	i = 0;
+	while (n >= sizeof(long))
+	{
+		*ptr++ = data;
+		n -= sizeof(long);
+	}
+	ptr2 = (char*)ptr;
+	while (n)
+	{
+		*ptr2++ = c;
+		n--;
+	}
+	return (ptr);
 }
